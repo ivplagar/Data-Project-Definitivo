315976da3f7b7bbc16795db8688cd74f59ced2d7
Ivan Pla Garcia <ivan.pla.1992@gmail.com>
2022-11-23 01:02:17 +0100

    s

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY ./requirements.txt /app/
 
 WORKDIR /app/
 
-RUN  pip install psycopg2
+RUN pip install psycopg2
 RUN pip install geojson
 RUN pip install -r requirements.txt
 RUN pip install pandas 
--- main.py
+++ main.py
@@ -6,14 +6,11 @@ import json
 import numpy as np
 import psycopg2.extras as extras
 
-  
-
-
-#URL= 'https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=hospitales&q=&facet=nombre&facet=financiaci&facet=tipo&facet=fecha&facet=barrio'
+URL= 'https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=hospitales&q=&facet=nombre&facet=financiaci&facet=tipo&facet=fecha&facet=barrio'
   
 #Obtenemos el paquete/caja que nos viene de ahi con el get'
 
-URL= 'https://valencia.opendatasoft.com/explore/dataset/centros-educativos-en-valencia/download/?format=json&timezone=Europe/Berlin&lang=es'
+#URL= 'https://valencia.opendatasoft.com/explore/dataset/centros-educativos-en-valencia/download/?format=json&timezone=Europe/Berlin&lang=es'
   
 #Obtenemos el paquete/caja que nos viene de ahi con el get'
 respuesta = requests.get(url=URL)
@@ -22,33 +19,22 @@ datos=respuesta.json()
 df = pd.json_normalize(datos)
 #CONEXION A POSTGREESQL
 
-connection = psycopg2.connect(user="postgres", password="Welcome01",host="localhost", port="5432", database="postgres")
+connection = psycopg2.connect(user="postgres", password="Welcome01",host="postgres", port="5432", database="postgres")
 cursor = connection.cursor()
 
 cursor.execute(
   """
     CREATE TABLE IF NOT EXISTS hospitales(
-    nombre varchar(50),
-    coddistrit integer);
+    nhits integer);
   """
 )
 
 for i in range(len(df)):  
   print(i)
 
-  postgres_insert_query = """ INSERT INTO hospitales (nombre, coddistrit) VALUES (%s,%s)"""
+  postgres_insert_query = """ INSERT INTO hospitales (nhits) VALUES (%s)"""
 
-  record_to_insert = (df['nombre'][i],df['coddistrit'][i])
+  record_to_insert = (df['nhits'][i])
   cursor.execute(postgres_insert_query, record_to_insert)
 
-
-
-
-
 connection.commit()
-
-
-
-
-
-
